13845847e3f228a7cce302ed9b409ff3b0d17712
Karim <karimsarkulov312@gmail.com>
2025-04-22 08:06:03 +0300

    chat

--- skillbridge-frontend/src/pages/MainPage.jsx
+++ skillbridge-frontend/src/pages/MainPage.jsx
@@ -3,6 +3,7 @@ import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 import Search from "../components/Search/Search";
 import MainIntro from "../components/MainIntro/MainIntro";
+import ChatButton from "../components/ChatButton/ChatButton";
 
 const MainPage = () => {
   return (
@@ -11,6 +12,7 @@ const MainPage = () => {
       <MainIntro />
       <Search />
       <Footer />
+      <ChatButton />
     </>
   );
 };
